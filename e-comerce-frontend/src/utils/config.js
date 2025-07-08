4c1230d15d917e16b9e2999bca9e76df3114ddb5
defhish <114762696+defhish@users.noreply.github.com>
2025-07-08 23:45:13 +0530

    Update config.js

--- e-comerce-frontend/src/utils/config.js
+++ e-comerce-frontend/src/utils/config.js
@@ -1,9 +1,5 @@
-const devConfig = {
+const config = {
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
 };
 
-const prodConfig = {
-  baseURL: import.meta.env.VITE_API_URL, // Use env var in prod too
-};
-
-export const config = import.meta.env.MODE === 'development' ? devConfig : prodConfig;
+export { config };
